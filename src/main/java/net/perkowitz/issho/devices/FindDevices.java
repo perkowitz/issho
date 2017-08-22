00446af3d7bb0761a6d8934c1803c8a8b95b7860
Mike Perkowitz <mike@perkowitz.net>
2017-08-22 07:53:31 -0700

    Added separate method to MidiUtil to list all midi devices

--- src/main/java/net/perkowitz/issho/devices/FindDevices.java
+++ src/main/java/net/perkowitz/issho/devices/FindDevices.java
@@ -15,17 +15,7 @@ public class FindDevices {
 
     public static void main(String args[]) throws Exception {
 
-        String[] names = new String[] { "NoSuchDevice" };
-        input = MidiUtil.findMidiDevice(names, false, true);
-        if (input == null) {
-            System.err.printf("Unable to find controller input device matching name: %s\n", StringUtils.join(names, ", "));
-            System.exit(1);
-        }
-//        output = MidiUtil.findMidiDevice(names, true, false);
-//        if (output == null) {
-//            System.err.printf("Unable to find controller output device matching name: %s\n", StringUtils.join(names, ", "));
-//            System.exit(1);
-//        }
+        MidiUtil.printMidiDevices();
 
     }
 
