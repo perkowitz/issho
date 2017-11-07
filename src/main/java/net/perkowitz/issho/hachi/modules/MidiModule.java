7ad36ba01b6ede89bf104bf1af4a872685c4592b
Mike Perkowitz <mike@perkowitz.net>
2017-11-06 22:07:10 -0800

    Send program changes even when module is muted

--- src/main/java/net/perkowitz/issho/hachi/modules/MidiModule.java
+++ src/main/java/net/perkowitz/issho/hachi/modules/MidiModule.java
@@ -94,8 +94,6 @@ public class MidiModule extends BasicModule implements Receiver {
     protected void sendMidiProgramChange(int channel, int value) {
 //        System.out.printf("Prog: %d, %d\n", channel, value);
 
-        if (isMuted && value > 0) return;
-
         try {
             ShortMessage message = new ShortMessage();
             message.setMessage(ShortMessage.PROGRAM_CHANGE, channel, value, 0);
